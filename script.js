9cc2e98cf682f80ca20ada8b15308316d4e68849
Pauline Pettersson <pauline-pettersson@hotmail.com>
2025-12-02 08:39:26 +0100

    json-file with quiz questions and start js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 'use strict';
 
+let quizContainer = document.querySelector('.quiz-container');
+let playBtn = document.querySelector('.play');
+
+
+// spara användarens input namn
+// när användaren klickar på play så ska innerHTML bytas ut 
+// hämta frågor och svar från JSON dokument
+// använd litteral strings för att skriva ut ny innerHTML
+
+playBtn.addEventListener('click', function () {
+    let userName = document.querySelector('#name').value;
+    console.log(userName);
+    quizContainer.innerHTML = `<header>
+            <h1>${userName}'s QUIZ</h1>
+            <div class="progress-bar" style="width: ${jsonElement}%"></div>
+        </header>`;
+})
